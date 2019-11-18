6de81aae8438f8ce85b08fd0c032a6f1224771b5
杨辉 <hui.yang@handeson.com>
2019-11-18 10:00:40 +0800

    增加配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        'semi': ['error', 'always'], // 分号结尾
+        'quotes': [1, 'single'], // 强制单引号
+        'indent': ['error', 4, { 'SwitchCase': 1 }], // 4空格缩进
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    devServer: {
+        proxy: 'http://testmanage.handeson.com/api'
+    }
+};
