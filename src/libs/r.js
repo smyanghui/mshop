c4a9542fe71ae54e113ed2401ed35a1d5fab6b33
杨辉 <hui.yang@handeson.com>
2019-11-22 16:29:20 +0800

    aa

--- src/libs/r.js
+++ src/libs/r.js
@@ -1,33 +1,33 @@
 import axios from 'axios';
-
-// axios.defaults.baseURL = '//testapi.handeson.com';
-// axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
-// export default axios;
-
-const apiDomain = {
-    dev: '//testapi.handeson.com',
-    prd: '//prdapi.handeson.com'
-};
-const env = process.env.VUE_APP_CURENV || 'prd';
-const baseURL = apiDomain[env];
+import domain from '@/config/domain';
 
 export default {
-    ajax (url, data, method = 'post') {
+    raxios (param) {
         return new Promise((resolve, reject) => {
-            axios({
-                baseURL,
-                url,
-                data,
-                method,
-                headers: {
-                    'app-platform': 'mp',
-                    'app-v': '2.0.0'
-                }
-            }).then((res) => {
+            axios(param).then((res) => {
                 resolve(res.data);
             }).catch((error) => {
                 reject(error);
             });
         });
+    },
+    ajax (ourl, data, method = 'post') {
+        const apiDomain = domain.mall[domain.env]; // 默认卖场
+        const url = /^https?:\/\//i.test(ourl) ? ourl : (apiDomain + ourl);
+        const param = {
+            url,
+            data,
+            method,
+            headers: {
+                'app-platform': 'mp',
+                'app-v': '2.0.0'
+            }
+        };
+
+        return this.raxios(param);
     }
 };
+
+// axios.defaults.baseURL = '//testapi.handeson.com';
+// axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+// export default axios;
