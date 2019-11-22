4634c997a035cc2a40f742c1ee9a62891e76e282
杨辉 <hui.yang@handeson.com>
2019-11-22 18:07:53 +0800

    abc

--- src/libs/r.js
+++ src/libs/r.js
@@ -1,76 +1,33 @@
 import axios from 'axios';
+import domain from '@/config/domain';
 
-let callback;
-
-function override (f) {
-    return function (...args) {
+export default {
+    raxios (param) {
         return new Promise((resolve, reject) => {
-            let start = Date.now();
-
-            f(...args).then((res) => {
-                callback(res.data, resolve, reject);
-                doMonitors(start, args, res.data);
-            }, (res) => {
-                reject(res.data);
-                doMonitors(start, args, res.data);
+            axios(param).then((res) => {
+                resolve(res.data);
+            }).catch((error) => {
+                reject(error);
             });
         });
-    };
-}
-
-let r = override(axios);
-let m = 'request get delete head options post put patch'.split(' ');
-
-m.forEach((method) => {
-    r[method] = override(axios[method]);
-});
-
-for (let i in axios) {
-    if (m.indexOf(i) > -1) {
-        r[i] = override(axios[i]);
-    } else {
-        r[i] = axios[i];
-    }
-}
-
-r.install = function (Vue, cfg) {
-    r.config(cfg);
-    Vue.prototype.$ajax = Vue.prototype.$fetch = Vue.prototype.$axios = r;
-};
-
-let monitors = [
-    function (start, params, data) {
-        if (Date.now() - start > 500) {
-            console.log('%c 请求时长' + (Date.now() - start) + 'ms, 请求参数为：', 'background: red; color: #fff;', params);
-        }
-    }
-];
-
-function doMonitors (start, params, data) {
-    monitors.forEach((monitor) => {
-        monitor(start, params, data);
-    });
-}
-
-r.monitor = function (monitor) {
-    monitors.push(monitor);
-};
-
-r.config = function (cfg = {}) {
-    if (cfg.callback) {
-        callback = cfg.callback;
-        delete cfg.callback;
-    }
-
-    for (let key in cfg) {
-        if (axios.defaults[key] && axios.defaults[key].constructor == Object) {
-            for (let sk in cfg[key]) {
-                axios.defaults[key][sk] = cfg[key][sk];
+    },
+    ajax (ourl, data, method = 'post') {
+        const apiDomain = domain.mall[domain.env]; // 默认卖场
+        const url = /^https?:\/\//i.test(ourl) ? ourl : (apiDomain + ourl);
+        const param = {
+            url,
+            data,
+            method,
+            headers: {
+                'app-platform': 'mp',
+                'app-v': '2.0.0'
             }
-        } else {
-            axios.defaults[key] = cfg[key];
-        }
+        };
+
+        return this.raxios(param);
     }
 };
 
-export default r;
+// axios.defaults.baseURL = '//testapi.handeson.com';
+// axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+// export default axios;
--- src/router/order/index.js
+++ src/router/order/index.js
@@ -1,13 +1,9 @@
 import Manage from './manage';
-import Setting from './set';
 import AfterSale from './afterSale';
-import Compensation from './compensation';
 
-import _ from '../util';
+import util from '../util';
 
 export default [
-    _.fill(Manage),
-    _.fill(Setting),
-    _.fill(AfterSale),
-    _.fill(Compensation)
+    util.fill(Manage),
+    util.fill(AfterSale)
 ];
